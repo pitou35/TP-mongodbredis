26c22a263d533b15a3487db78bd53a8ba0d32e97
PAT <pierreaugustin.totaro03@gmail.com>
2016-03-09 14:37:36 +0100

    first commit

---
+++ src/main/java/test/TP7Sir/App.java
@@ -0,0 +1,13 @@
+package test.TP7Sir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
